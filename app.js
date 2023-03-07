299f6e9657dc59d720628a63595a16037bbba20b
cenzo <100490521+vgalante2@users.noreply.github.com>
2023-03-07 14:24:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
 const https = require("https");
-const ("dotenv").config();
+require("dotenv").config();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
